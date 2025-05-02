a91cc206efd5aab513ae9233fd90e0b2e04f34ab
Gestalte <66849891+Gestalte@users.noreply.github.com>
2025-05-02 18:27:22 +0200

    Update z-lib link

--- src/StartPage.elm
+++ src/StartPage.elm
@@ -103,7 +103,7 @@ downloads =
           , ( "https://1337x.to", "1337x" )
           , ( "https://nyaa.si", "Nyaa" )
           ]
-        , [ ( "https://z-lib.gs/", "z-lib" )
+        , [ ( "https://z-library.sk/", "z-lib" )
           , ( "https://annas-archive.org/", "Anna's Archive" )
           , ( "https://www.dbooks.org/", "dBooks Library" )
           , ( "https://audiobookbay.lu", "AudiobookBay" )
